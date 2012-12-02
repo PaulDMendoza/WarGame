c5130199cfe5074e2887fbd1f6ff4f6aecc2ae1d
Paul Mendoza <paul@pauldmendoza.com>
2012-12-01 16:42:50 -0800

    Sniper image positioned correctly in middle

--- Site/Scripts/Game/Entities/Sniper.ts
+++ Site/Scripts/Game/Entities/Sniper.ts
@@ -1,41 +1,36 @@
 /// <reference path="../Entity.ts" />
 
 module Game {
-    export class Sniper extends Entity {               
-        
-        constructor (config : IEntityConfiguration) {
-            super(config); 
+    export class Sniper extends Entity {
+
+        constructor (config: IEntityConfiguration) {
+            super(config);
         }
 
         canMove() {
             return true;
         }
-        
+
         getKineticGroup() {
             var self = this;
             var group = super.getKineticGroup();
-            
-            var imageObj = new Image();
-            imageObj.onload = function () {
-                var image = new Kinetic.Image({
-                    image: imageObj,
-                    width: 48,
-                    height: 48                    
-                });
 
-                image.on('click', function () {
-                    self.move(self._group.getX() + 50, self._group.getY() + 50, {
-                        pixelsPerSecond: 10
-                    });
-                });
+            var soldier = this.addImage({
+                url: "/Images/GameAssets/Soldiers/Sniper.png",
+                width: 48,
+                height: 48,
+                onLoadPostDraw: function () {
 
-                group.add(image);
-                self.draw();
-                console.log("Drawing soldier");
-            };
-            imageObj.src = "/Images/GameAssets/Soldiers/Sniper.png";
-            
+                }
+            });
+
+            soldier.KineticImage.on('click', function () {
+                self.move(self._group.getX() + 50, self._group.getY() + 50, {
+                    pixelsPerSecond: 10
+                });
+            });
+                        
             return group;
-        }                                       
+        }
     }
 }
